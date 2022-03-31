98400f28467b675d3b510ec0b4aeb2c2641d0506
zuma990 <zumajuve@gmail.com>
2022-03-31 18:05:32 +0200

    Added new file

---
+++ ex07/src/index.js
@@ -0,0 +1,15 @@
+function main(numOne, numTwo) {
+  // Only change code below this line
+
+  var remainder;
+  var numOne;
+  var numTwo;
+
+  remainder = numOne % numTwo;
+  // Only change code above this line
+
+  return remainder;
+}
+
+console.log(main(102, 25));
+module.exports = main;
