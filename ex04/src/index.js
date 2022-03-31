5d3c5724c098f710b43eb3d55fe844e68f886224
zuma990 <zumajuve@gmail.com>
2022-03-31 17:29:45 +0200

    Added new file

---
+++ ex04/src/index.js
@@ -0,0 +1,12 @@
+function main() {
+  // Only change code below this line
+  var myDecimal = 25.4;
+  var myDecimalE = 254e-5;
+  // Only change code above this line
+  return {
+    myDecimal,
+    myDecimalE,
+  };
+}
+console.log(main());
+module.exports = main;
