b53d26850e94218be4bfb21819746901ce62d433
zuma990 <zumajuve@gmail.com>
2022-03-31 17:42:40 +0200

    Added new file

---
+++ ex06/src/index.js
@@ -0,0 +1,13 @@
+function main(numOne, numTwo) {
+  // Only change code below this line
+
+  var numOne;
+  var numTwo;
+  var myQuotient = numOne / numTwo;
+  // Only change code above this line
+
+  return myQuotient;
+}
+
+console.log(main(102.0, 25.5));
+module.exports = main;
