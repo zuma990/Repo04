9e03a62b51e18c58b9b5ecfab5f26083bb8db502
zuma990 <zumajuve@gmail.com>
2022-03-31 18:25:22 +0200

    Added new file

---
+++ ex09/src/index.js
@@ -0,0 +1,22 @@
+function main() {
+  // Only change code below this line
+
+  var firstNum = 6;
+  var secondNum = 25;
+  var thirdNum = 5.2;
+
+  firstNum *= 9;
+  thirdNum *= 10;
+  secondNum /= 5;
+
+  // Only change code above this line
+
+  return {
+    firstNum,
+    secondNum,
+    thirdNum,
+  };
+}
+
+console.log(main());
+module.exports = main;
