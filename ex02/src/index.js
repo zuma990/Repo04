52b7ed6e98e258c8359051c6b89c78db819dcea1
zuma990 <zumajuve@gmail.com>
2022-03-31 17:02:52 +0200

    Added new file

---
+++ ex02/src/index.js
@@ -0,0 +1,9 @@
+function main() {
+  var myRes = 75;
+  // Only change code below this line
+  myRes++;
+  // Only change code above this line
+  return myRes;
+}
+console.log(main());
+module.exports = main;
