e9aff35a4361867d9d54daf7bc1e4d78fb27eb5f
zuma990 <zumajuve@gmail.com>
2022-03-31 17:37:43 +0200

    Added new file

---
+++ ex05/src/index.js
@@ -0,0 +1,13 @@
+function main(numOne, numTwo) {
+  // Only change code below this line
+
+  var numOne;
+  var numTwo;
+  var product = numOne * numTwo;
+  // Only change code above this line
+
+  return product;
+}
+
+console.log(main(100.25, 43.2));
+module.exports = main;
